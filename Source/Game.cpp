a9eeacd8796c88e1842f46a517dd790d0c38f61d
AlonsoNunez7Av <nezalon@hotmail.com>
2014-09-22 10:05:55 -0700

    Corrigiendo error
    Se cambia estado inicial

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -2,8 +2,30 @@
 #include <stdlib.h>
 #include "Game.h"
 
-CGame::CGame(){
+CGame::CGame()
+
+{
 	int estado = 0; ///ACT2: Aqui debes de iniciar tu estado, con el nuevo estado inicial. Esto que tienes no es correcto.
+	//Maquina de estados
+
+	switch (estado){
+	case Estado::ESTADO_INICIANDO: //INICIALIZAR
+		break;
+
+	case Estado::ESTADO_MENU:	//JUGAR	
+		break;
+
+	case Estado::ESTADO_JUGANDO:	//JUGAR	
+		break;
+
+	case Estado::ESTADO_TERMINANDO:	//JUGAR	
+		break;
+
+	case Estado::ESTADO_FINALIZANDO: //SALIR
+		break;
+
+		
+	};
 }
 
 // Con esta función eliminaremos todos los elementos en pantalla
@@ -16,27 +38,10 @@ bool CGame::Start()
 	int salirJuego = false;
           
 	while (salirJuego == false){
-            
-		//Maquina de estados
-		switch(estado){
-			case Estado::ESTADO_INICIANDO: //INICIALIZAR
-				break;
-
-			case Estado::ESTADO_MENU:	//JUGAR	
-				break;
-
-			case Estado::ESTADO_JUGANDO:	//JUGAR	
-				break;
-
-			case Estado::ESTADO_TERMINANDO:	//JUGAR	
-				break;
-
-			case Estado::ESTADO_FINALIZANDO: //SALIR
-				break;
-
-				salirJuego = true;
-			break;
-		};
+        
+		salirJuego = true;
+		break;
+		
     }
 	return true;
 }

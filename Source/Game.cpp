b617f19a337ac0361d76811b87ced0d0e16237f5
AlonsoNunez7Av <nezalon@hotmail.com>
2014-09-22 18:39:17 -0700

    Corrigiendo
    Corrigiendo "int estado = 0;"
    por
    "estado = ESTADO_INICIANDO;"

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -5,16 +5,28 @@
 CGame::CGame()
 
 {
-	int estado = 0; 
+	estado = ESTADO_INICIANDO; 
 	
 	///ACT2:Mal, Debes de poner que estado sera el inicial, no debiste mover el motor de estados, ese estaba bien en su lugar, dentro del While, para que siempre
 	//Se este ejecutando. Pero es necesario que el juego le indiques que estado sera el primero en ejecutarse. 
 	//El int estado = 0; No debe de llevar el int.
 	//Dejalo como esta y solo compone lo de la parte de arriba "int estado = 0;"
+	
+}
 
-	//Maquina de estados
+// Con esta función eliminaremos todos los elementos en pantalla
+void CGame::Finalize(){
+}
 
-	switch (estado){
+bool CGame::Start()
+{
+	// Esta variable nos ayudara a controlar la salida del juego...
+	int salirJuego = false;
+          
+	while (salirJuego == false){
+
+		//Maquina de estados       
+		switch (estado){
 	case Estado::ESTADO_INICIANDO: //INICIALIZAR
 		break;
 
@@ -30,23 +42,10 @@ CGame::CGame()
 	case Estado::ESTADO_FINALIZANDO: //SALIR
 		break;
 
-		
-	};
-}
-
-// Con esta función eliminaremos todos los elementos en pantalla
-void CGame::Finalize(){
-}
-
-bool CGame::Start()
-{
-	// Esta variable nos ayudara a controlar la salida del juego...
-	int salirJuego = false;
-          
-	while (salirJuego == false){
-        
 		salirJuego = true;
 		break;
+
+		};
 		
     }
 	return true;

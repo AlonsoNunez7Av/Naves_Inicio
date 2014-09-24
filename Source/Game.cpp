a1b72124e960a3b45112a09c5bd395264903a962
AlonsoNunez7Av <nezalon@hotmail.com>
2014-09-23 19:25:15 -0700

    Actividad3
    Terminando actividad tres, usando SDL's

--- Source/Game.cpp
+++ Source/Game.cpp
@@ -5,17 +5,19 @@
 #include <SDL.h>
 
 CGame::CGame(){
-	
-	estado = ESTADO_INICIANDO; 
-	SDL_Surface * screen;
+	estado = ESTADO_INICIANDO;
 	atexit(SDL_Quit);
 
-	if (SDL_Init(SDL_INIT_VIDEO)<0){
+	
+	
+
+	if (SDL_Init(SDL_INIT_VIDEO)){
 		printf("Error %s ", SDL_GetError());
 		exit(EXIT_FAILURE);	
 
-		screen = SDL_SetVideoMode(640, 480, 24, SDL_SWSURFACE);
 	}
+
+	screen = SDL_SetVideoMode(640, 480, 24, SDL_SWSURFACE);
 	    	    
 	if (screen == NULL){
 		printf("Error %s ", SDL_GetError());
@@ -37,11 +39,35 @@ bool CGame::Start()
 	// Esta variable nos ayudara a controlar la salida del juego...
 	int salirJuego = false;
           
-	while (salirJuego == false){
+	while (salirJuego == false){		
 
 		//Maquina de estados       
 		switch (estado){
-	case Estado::ESTADO_INICIANDO: //INICIALIZAR
+		case Estado::ESTADO_INICIANDO: //INICIALIZAR......Estamos arrancando con el juego 
+		{
+										   nave = SDL_LoadBMP("../Data/MiNave.bmp");//Busca la imagen en las carpetas del programa
+										 //nave = IMG_LoadBMP_RW(SDL_RWFromFile("../Data/Tools.jpg", "rb")); //jala otra imagen
+										   
+
+										   SDL_Rect Fuente;  // esta sera la posicion de la imagen que queremos imprimir en pantalla
+										   Fuente.x = 90;
+										   Fuente.y = 152;
+										   Fuente.w = 242;
+										   Fuente.h = 76;
+
+										   SDL_Rect destino; // aqui le desimos en que posicion queremos que imprima en pantalla
+										   destino.x = 100;
+										   destino.y = 100;
+										   destino.w = 100;
+										   destino.h = 100;
+
+										   SDL_BlitSurface(nave, &Fuente, screen, &destino);//Pasar la imagen a la pantalla principal
+
+										   SDL_BlitSurface(nave, NULL, screen, NULL);//Pasar la imagen a la pantalla principal
+
+										   SDL_FreeSurface(nave); // Limpia la imagen si no la ocupamos
+		}
+
 		break;
 
 	case Estado::ESTADO_MENU:	//JUGAR	
@@ -60,7 +86,8 @@ bool CGame::Start()
 		break;
 
 		};
-		SDL_Flip(screen);
+		
+		SDL_Flip(screen);// imprimir en pantalla la variable screen
 		
     }
 	return true;
--- Source/Game.h
+++ Source/Game.h
@@ -20,6 +20,8 @@ public:
 
 private:
 	void Iniciando(); 
-	SDL_Surface * screen;
+	SDL_Surface *screen;
+	SDL_Surface *nave;
+
 	Estado estado;
 };
